b112c402b5ca7f37071a54ff84f238904476bda3
Hanssen <hramanjooloo@gmail.com>
2020-07-18 21:32:17 +0100

    finished functionality for this project

--- index.js
+++ index.js
@@ -2,52 +2,72 @@
 const drums = document.querySelectorAll("button.drum");
 
 // Constructor function for drum objects
-function Drum(audio, keyNumber) {
+function DrumObject(audio, keyNumber, element) {
   this.audio = audio;
-  this.keyNumber = keyNumber;
 }
 
 // Drum objects
-const wDrum = new Drum(new Audio("sounds/tom-1.mp3"), 87);
-const aDrum = new Drum(new Audio("sounds/tom-2.mp3"), 65);
-const sDrum = new Drum(new Audio("sounds/tom-3.mp3"), 83);
-const dDrum = new Drum(new Audio("sounds/tom-4.mp3"), 68);
-const jDrum = new Drum(new Audio("sounds/crash.mp3"), 74);
-const kDrum = new Drum(new Audio("sounds/kick-bass.mp3"), 75);
-const lDrum = new Drum(new Audio("sounds/snare.mp3"), 76);
+const wDrum = new DrumObject(new Audio("sounds/tom-1.mp3"));
+const aDrum = new DrumObject(new Audio("sounds/tom-2.mp3"));
+const sDrum = new DrumObject(new Audio("sounds/tom-3.mp3"));
+const dDrum = new DrumObject(new Audio("sounds/tom-4.mp3"));
+const jDrum = new DrumObject(new Audio("sounds/crash.mp3"));
+const kDrum = new DrumObject(new Audio("sounds/kick-bass.mp3"));
+const lDrum = new DrumObject(new Audio("sounds/snare.mp3"));
 
 // Loop through the drums array and add a click event to each element
-for (var i = 0; i < drums.length; i++) {
+for (let i = 0; i < drums.length; i++) {
   drums[i].addEventListener("click", function () {
     // Store text of drums
     let drumText = this.innerHTML;
 
-    // Play a drum audio file based on which drum was clicked
-    switch (drumText) {
-      case "w":
-        wDrum.audio.play();
-        break;
-      case "a":
-        aDrum.audio.play();
-        break;
-      case "s":
-        sDrum.audio.play();
-        break;
-      case "d":
-        dDrum.audio.play();
-        break;
-      case "j":
-        jDrum.audio.play();
-        break;
-      case "k":
-        kDrum.audio.play();
-        break;
-      case "l":
-        lDrum.audio.play();
-        break;
-      default:
-        console.log(drumText);
-        break;
-    }
+    playDrum(drumText);
+
+    buttonAnimation(drumText);
   });
+}
+
+// listens for key presses and runs the playDrum() function
+document.addEventListener("keydown", function (event) {
+  playDrum(event.key);
+
+  buttonAnimation(event.key);
+});
+
+function playDrum(key) {
+  // Play a drum audio file based on which drum was clicked or button pressed
+  switch (key) {
+    case "w":
+      wDrum.audio.play();
+      break;
+    case "a":
+      aDrum.audio.play();
+      break;
+    case "s":
+      sDrum.audio.play();
+      break;
+    case "d":
+      dDrum.audio.play();
+      break;
+    case "j":
+      jDrum.audio.play();
+      break;
+    case "k":
+      kDrum.audio.play();
+      break;
+    case "l":
+      lDrum.audio.play();
+      break;
+    default:
+      console.log("Wrong key!");
+      break;
+  }
+}
+
+function buttonAnimation(currentKey) {
+  const activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
